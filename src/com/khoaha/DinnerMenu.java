cb1235afbd30f53c0841821cdec1293f0642a316
Khoa <khoaha@teusoft.dk>
2015-12-30 10:22:45 +0700

    first commit. There're 2 menu: PancakeHouseMenu using data type is ArrayList for storing menus and DinnerMenu using Array.

---
+++ src/com/khoaha/DinnerMenu.java
@@ -0,0 +1,32 @@
+package com.khoaha;
+
+/**
+ * Created by HoangAnhKhoa on 12/30/15.
+ */
+public class DinnerMenu {
+    MenuItem[] menuItems;
+    static final int MAX_ITEM = 6;
+    int numberOfItems = 0;
+
+    public DinnerMenu() {
+        menuItems = new MenuItem[MAX_ITEM];
+
+        addItem("Vegetarian BLT" , "Vegetarian BLT Desc" , true, 2.99);
+        addItem("BLT" , "Bacon with lettuce" , false, 2.79);
+        addItem("Soup of the day" , "Soup of the day with potato and salad" , false, 3.29);
+        addItem("Hot dog" , "Hot dog with saurkrant, relish, onions, topped with cheese" , false, 3.29);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        if (numberOfItems < MAX_ITEM) {
+            menuItems[numberOfItems] = new MenuItem(name, description, vegetarian, price);
+            numberOfItems++;
+        }else{
+            System.out.println("Sorry you can not add more item.");
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
